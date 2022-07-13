7fbf45462fe7c097a7cb947b4a3afa39b3f38078
Võ Minh Trí <89436428+FlyTri@users.noreply.github.com>
2022-07-13 11:54:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (process.env.PORT) {
     const fs = require("fs")
     let application = fs.readFileSync("./application.yml", "utf8")
-    application = application.replace("$PORT", process.env.PORT)
+    application = application.replace("443", process.env.PORT)
     fs.writeFileSync("./application.yml", application)
 }
