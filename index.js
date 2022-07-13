10343a705b0bea6cbccb24eb02d7407e1f76fca3
Võ Minh Trí <89436428+FlyTri@users.noreply.github.com>
2022-07-13 11:59:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require("fs")
 let application = fs.readFileSync("./application.yml", "utf8")
 
-if (process.env.PORT) {
+if (process.env.PORT !== undefined) {
     application = application.replace("$PORT", process.env.PORT)
+    console.log("Start on port: " + process.env.PORT)
 } else {
     application = application.replace("$PORT", 443)
+    console.log("Start on port: 443")
 }
 
 fs.writeFileSync("./application.yml", application)
